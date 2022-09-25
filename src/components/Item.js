879e18c69aae2901717e0a4f869442115796626c
JUdihouma04 <sheldonodhiuma04@gmail.com>
2022-09-25 20:50:20 +0300

    completed items

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState} from "react";
 
 function Item({ name, category }) {
+  function  handleClick() {
+    setClassName(()=> !inCart)
+  }
+  const [inCart, setClassName] = useState(false)
   return (
-    <li className="">
+    <li className= {inCart ? "in-cart": ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onCLick={handleClick} className="add">{inCart ? "Remove From Cart" : "Add To Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setselectedCategory] = useState(items)
+  function handleChange(event){
+    const cart =event.target.value;
+    const cartItems =items.filter((item)=> items.category ===cart)
+    setselectedCategory(cartItems)
+  }
   return (
     <div className="ShoppingList">
       <div className="Filter">
